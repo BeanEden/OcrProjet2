b93edb279cc09d0c1d4fcf2cc42e270bd6f6b649
JC <bean3den@gmail.com>
2021-12-03 18:27:46 +0100

    fonction extractions fully functional

--- burner_cal.py
+++ burner_cal.py
@@ -6,6 +6,102 @@ urlcategorie = "http://books.toscrape.com/catalogue/category/books/mystery_3/ind
 
 
 
+# Correction des listes
+# def deleteIndex(index):
+#     del informations[index]
+#     del informationsVal[index]
+
+# Intégration dans le fichier CSV
+# def écriture():
+#     with open('data.csv', 'w', newline="") as csv_file:
+#     dw = csv.DictWriter(csv_file, delimiter=',', fieldnames=informations)
+#     dw.writeheader()
+#     dw.writerow(tableauFinal)
+def categorieFinder():
+    url = "http://books.toscrape.com/index.html"
+    completURL = "http://books.toscrape.com/"
+    reponse = requests.get(url)
+    page = reponse.content
+    soup = bs(reponse.content, "html.parser")
+    #
+    # print(soup.prettify())
+
+    listeCategorie = []
+    boxLivres = soup.find('ul', class_='nav')
+
+    for a in boxLivres.find_all('a', href=True):
+        # print("Found the URL:", a['href'])
+        listeCategorie.append(completURL + a['href'])
+    del listeCategorie[0]
+    return(listeCategorie)
+
+def fpageIndex(urlPage):
+    reponse = requests.get(urlPage)
+    page = reponse.content
+    soup = bs(page, "html.parser")
+    boxPage = soup.find('li', class_='next')
+    pageIndex = ""
+    if boxPage is not None :
+       for a in boxPage.find_all('a', href=True):
+            pageIndex = (a['href'])
+    else :
+        pageIndex = None
+    return(pageIndex)
+
+def urlLivresCategorie(urlCategorie):
+    # root = url.replace("index.html","")
+    root = "http://books.toscrape.com/catalogue/"
+    reponse = requests.get(urlCategorie)
+    page = reponse.content
+    soup = bs(page, "html.parser")
+
+    boxLivres = soup.find('ol', class_='row')
+    listeCategorieIntermediaire = []
+
+    for a in boxLivres.find_all('a', href=True):
+        # print("Found the URL:", a['href'])
+        listeCategorieIntermediaire.append(a['href'])
+
+    def noDoublon(x):
+        return list(dict.fromkeys(x))
+
+    listeCategorie = noDoublon(listeCategorieIntermediaire)
+
+    def fullUrl(x):
+        listeUrl = []
+        for livres in x:
+            listeUrl.append(livres.replace("../../../", root))
+        return listeUrl
+
+    listetotale = (fullUrl(listeCategorie))
+    pageN = fpageIndex(urlCategorie)
+
+    while pageN is not None:
+        urlNewPage = urlCategorie.replace("index.html", pageN)
+        pageN = fpageIndex(urlNewPage)
+        listetotale.append(urlNewPage)
+
+    return listetotale
+
+
+def bouclePagination(urlpagination):
+
+    listetotale = urlLivresCategorie(urlpagination)
+    pageN = fpageIndex(urlpagination)
+
+    while pageN is not None:
+        urlNewPage = urlpagination.replace("index.html", pageN)
+        pageN = fpageIndex(urlNewPage)
+        listetotale.extend(urlLivresCategorie(urlNewPage))
+
+    return listetotale
+
+def lectureCategorie(urlCatFull):
+    urlPage1= urlLivresCategorie(urlCatFull)
+    urlPages=bouclePagination(urlCatFull)
+    completeList = urlPage1 + urlPages
+    return completeList
+
 def etlPage(urlPage):
     informations = []
     reponse = requests.get(urlPage)
@@ -64,80 +160,9 @@ def etlValeurs(urlPageVal):
 
     return informationsVal
 
-def fpageIndex(urlPage):
-    reponse = requests.get(urlPage)
-    page = reponse.content
-    soup = bs(page, "html.parser")
-    boxPage = soup.find('li', class_='next')
-    pageIndex = ""
-    if boxPage is not None :
-       for a in boxPage.find_all('a', href=True):
-            pageIndex = (a['href'])
-    else :
-        pageIndex = None
-    return(pageIndex)
-
-def bouclePagination(urlpagination):
-
-    listetotale = urlLivresCategorie(urlpagination)
-    pageN = fpageIndex(urlpagination)
-
-    while pageN is not None:
-        urlNewPage = urlpagination.replace("index.html", pageN)
-        pageN = fpageIndex(urlNewPage)
-        listetotale.append(urlNewPage)
-
-    return listetotale
 
 
-def urlLivresCategorie(urlCategorie):
-    # root = url.replace("index.html","")
-    root = "http://books.toscrape.com/catalogue/"
-    reponse = requests.get(urlCategorie)
-    page = reponse.content
-    soup = bs(page, "html.parser")
-
-    # print(soup)
-
-    # html = '''<a href="some_url">next</a>
-    # <span class="class"><a href="another_url">later</a></span>'''
-    #
-    # soup = BeautifulSoup(html)
-
-    boxLivres = soup.find('ol', class_='row')
-    listeCategorieIntermediaire = []
-
-    for a in boxLivres.find_all('a', href=True):
-        # print("Found the URL:", a['href'])
-        listeCategorieIntermediaire.append(a['href'])
-
-    def noDoublon(x):
-        return list(dict.fromkeys(x))
-
-    listeCategorie = noDoublon(listeCategorieIntermediaire)
-
-    def fullUrl(x):
-        listeUrl = []
-        for livres in x:
-            listeUrl.append(livres.replace("../../../", root))
-        return listeUrl
-
-    # def fpageIndex():
-    #     boxPage = soup.find('li', class_='next')
-    #     for a in boxPage.find_all('a', href=True):
-    #         pageIndex = (a['href'])
-    #     print(pageIndex)
-
-    return(fullUrl(listeCategorie))
-
-
-
-
-
-# print(urlLivresCategorie(urlcategorie))
-# print(etlPage(url))
-print(etlValeurs(url))
 print(categorieFinder())
-# print(bouclepagination(urlcategorie))
-# print(len(listetotale))
-# print(len(bouclepagination(urlcategorie)))
+print(lectureCategorie(urlcategorie))
+print(etlPage(url))
+print(etlValeurs(url))
